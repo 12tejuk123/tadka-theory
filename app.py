894b2adc43811ac83b1f74380981d541beee342b
12tejuk123 <tejaswini2504@gmail.com>
2026-05-17 21:26:40 +0530

    upgraded to sqlite backend

--- app.py
+++ app.py
@@ -4,48 +4,49 @@ import os
 
 app = Flask(__name__)
 
-ORDERS_FILE = "orders.json"
+ORDERS_FILE = 'orders.json'
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/place-order", methods=["POST"])
+@app.route('/place-order', methods=['POST'])
 def place_order():
 
     data = request.json
 
     order = {
-        "name": data["name"],
-        "phone": data["phone"],
-        "address": data["address"],
-        "product": data["product"],
-        "quantity": data["quantity"]
+        'name': data['name'],
+        'phone': data['phone'],
+        'address': data['address'],
+        'product': data['product'],
+        'quantity': data['quantity']
     }
+
 
     orders = []
 
     if os.path.exists(ORDERS_FILE):
-
-        with open(ORDERS_FILE, "r") as file:
-
+        with open(ORDERS_FILE, 'r') as file:
             try:
                 orders = json.load(file)
-
             except:
                 orders = []
 
+
     orders.append(order)
 
-    with open(ORDERS_FILE, "w") as file:
+
+    with open(ORDERS_FILE, 'w') as file:
         json.dump(orders, file, indent=4)
 
+
     return jsonify({
-        "message": "Order Placed Successfully!"
+        'message': 'Order placed successfully!'
     })
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
